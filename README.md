c26baead5b6a4465a3bc81dab9c4a6c7009ad03e
Nguyen Giang Chuong <42799279+ngchuong@users.noreply.github.com>
2021-01-18 15:32:25 +0700

    1st

---
+++ README.md
@@ -0,0 +1,1 @@
+Add style to html 
---
+++ WriteByJs/index.js
@@ -0,0 +1,12 @@
+function Spinner() {
+  return (
+    <div className="sk-chase">
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+    </div>
+  );
+}
